35670cba824ad593e4cdb5b46b71bb4504561d1b
Linh1605 <hoanglinh16052001@gmail.com>
2022-06-19 23:11:29 +0700

    Linh commit giao diện đăng ký

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-//Route::get('/', function () {
-    //return view('main');
-//});
+Route::get('/register', function () {
+    return view('registration');
+});
 Route::get('/login', function () {
     return view('login');             
 });
